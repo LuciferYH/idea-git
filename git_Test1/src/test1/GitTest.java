10b7336752063434d8dfcc0a9a8f5f1f970cbb8b
Lucifer <CNLuciferYH@gmail.com>
2020-05-09 08:37:19 +0800

    gitTest工程中添加了一个方法

--- git_Test1/src/test1/GitTest.java
+++ git_Test1/src/test1/GitTest.java
@@ -5,10 +5,16 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git test");
         System.out.println("git test");
+        System.out.println("测试差异");
     }
 
     public int count(){
         int i = 10;
         return i+1;
     }
+
+    public void test2(){
+        System.out.println("test");
+    }
+
 }
